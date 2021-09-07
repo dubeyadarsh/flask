98ff21b0780c405289586ecc37c317bc4265577f
dubeyadarsh <75479397+dubeyadarsh@users.noreply.github.com>
2021-09-07 16:40:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
-from flask import Flask
+from flask import Flask,jsonify
 app = Flask(__name__)
-@app.route("/")
-def home():
-    return "Hello, Flask!"
+
+
+@app.route('/')
+def hello_world():
+    json_file = {}
+    json_file['query'] = 'hello_world'
+    return jsonify(json_file)
+
+
+if __name__ == '__main__':
+    app.run()
